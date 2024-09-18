82520fa286b4dbb53457a7681e8f9597f2502987
MissAll-Shooter <24amelinesn@roundwoodpark.co.uk>
2024-09-18 18:45:31 +0000

    added stuff yaey

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const button1 = document.querySelector('.button1');
 const button2 = document.querySelector('.button2');
 const button3 = document.querySelector('.button3');
 const button4 = document.querySelector('.button4');
-const hamborgor = document.querySelector('.hamborgor');
-const sidebar = document.querySelector('.sidebar')
 
 button1.addEventListener('click', () => {
    button1.classList.toggle('active');
@@ -19,9 +17,4 @@ button3.addEventListener('click', () => {
 
 button4.addEventListener('click', () => {
     button4.classList.toggle('activate')
-})
-
-hamborgor.addEventListener('click', () => {
-    sidebar.classList.toggle('open');
-    hamborgor.classList.toggle('open');
 })
--- sidebar.js
+++ sidebar.js
@@ -1,22 +1,5 @@
-const hamgorgor_html = `<button class="hamborgor">
-            <div class="hmbrgricn hmbrgricn1"></div>
-            <div class="hmbrgricn hmbrgricn2"></div>
-            <div class="hmbrgricn hmbrgricn3"></div>
-        </button>`;
-
-const sidebar_html = `
-        <div class="sidebar">
-            <a href="">Hello world</a>
-            <a>Hello world</a>
-            <a>Hello world</a>
-            <a>Hello world</a>
-            <a>Hello world</a>
-            <a>Hello world</a>
-            <a>Hello world</a>
-            <a>Hello world</a>
-            <a>Hello world</a>
-            <a>Hello world</a>
-        </div>`;
+const hamborgor_html = `<div class="hmbrgricn hmbrgricn1"></div><div class="hmbrgricn hmbrgricn2"></div><div class="hmbrgricn hmbrgricn3"></div>`;
+const sidebar_html = `<a href="/chooter/">Hello world</a><a>Hello world</a><a>Hello world</a><a>Hello world</a><a>Hello world</a><a>Hello world</a><a>Hello world</a><a>Hello world</a><a>Hello world</a><a>Hello world</a>`;
 
 const sidebar = document.createElement('div');
 const hamborgor = document.createElement('button');
@@ -24,7 +7,12 @@ const hamborgor = document.createElement('button');
 sidebar.classList.add('sidebar');
 hamborgor.classList.add('hamborgor');
 
-sidebar.textContent = sidebar_html;
-hamborgor.textContent = hamgorgor_html;
+sidebar.innerHTML = sidebar_html;
+hamborgor.innerHTML = hamborgor_html;
+
+hamborgor.addEventListener('click', () => {
+    sidebar.classList.toggle('open');
+    hamborgor.classList.toggle('open');
+});
 
 document.body.append(sidebar, hamborgor);
