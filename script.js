1db489a987466174ed6c8f8cd70ef7236ff044cc
MissAll-Shooter <24amelinesn@roundwoodpark.co.uk>
2024-09-27 13:27:37 +0000

    mifuyffdivfjidd

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const button2 = document.querySelector('.button2');
 const button3 = document.querySelector('.button3');
 const button4 = document.querySelector('.button4');
 const button5 = document.querySelector('.button5');
-
+const button100 = document.querySelector('.button100')
 button1.addEventListener('click', () => {
    button1.classList.toggle('active');
 });
