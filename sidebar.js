e9cc85dd507f818332cf35be76ecb419fab1c9ac
MissAll-Shooter <24amelinesn@roundwoodpark.co.uk>
2024-10-01 18:15:22 +0000

    misc

--- sidebar.js
+++ sidebar.js
@@ -1,5 +1,5 @@
 const hamborgor_html = `<div class="hmbrgricn hmbrgricn1"></div><div class="hmbrgricn hmbrgricn2"></div><div class="hmbrgricn hmbrgricn3"></div>`;
-const sidebar_html = `<a href="/"><img src="/SidebarImages/TutoringDodeleReal.png" id="geog"></a><a href="/chooter/"><img src="/SidebarImages/GeographyDodele.png" id="geog"></a><a href="/butonz/"><img src="/SidebarImages/ButtonsDodele.png" id="geog"></a><a href="/whyareyoureadingthis/"><img src="/SidebarImages/StoryDodele.png" id="geog"><img src="/MiscDodele.png" href="/misc/" id="geog"></a>`;
+const sidebar_html = `<a href="/"><img src="/SidebarImages/TutoringDodeleReal.png" id="geog"></a><a href="/chooter/"><img src="/SidebarImages/GeographyDodele.png" id="geog"></a><a href="/butonz/"><img src="/SidebarImages/ButtonsDodele.png" id="geog"></a><a href="/whyareyoureadingthis/"><img src="/SidebarImages/StoryDodele.png" id="geog"></a><a href="https://missall.my.canva.site/#page-1"><img src="/MiscDodele.png" href="https://missall.my.canva.site/#page-1" id="geog"></a>`;
 
 const sidebar = document.createElement('div');
 const hamborgor = document.createElement('button');
